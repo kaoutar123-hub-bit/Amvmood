2fc45a01c6c8e924ff853e727645dd6e7f90e66c
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-05-11 19:20:26 +0200

    modificando el cors

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'email/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'paths' => ['api/*', 'email/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
+    'allowed_methods' => ['*'],
     'allowed_origins' => ['https://react-eight-eosin.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
