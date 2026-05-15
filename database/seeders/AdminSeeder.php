e3db609cb39a5bc2cbdd31bd0a93c86b2f6702bf
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-05-15 19:51:44 +0200

    actualizando

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,8 +17,8 @@ class AdminSeeder extends Seeder
         // php artisan make:seeder 'AdminSeeder'
         User::forceCreate([
             'nickname' => 'admin',
-            'email'    => 'soloadmin@institutmvm.cat',
-            'password' => Hash::make('soloadmin123'),
+            'email'    => 'administrador@institutmvm.cat',
+            'password' => Hash::make('@dministr@d0r'),
             'rol'      => 'admin',
             'email_verified_at' => now(),
         ]);
