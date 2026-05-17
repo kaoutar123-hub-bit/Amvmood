92d93ee719f15cac67fbcb2cfdc0f72625004f5e
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-05-17 10:04:56 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'email/*', 'storage/', 'sanctum/csrf-cookie', 'broadcasting/auth'],
+    'paths' => ['api/*', 'email/*', 'storage/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://mvmood.com'],
     'allowed_origins_patterns' => [],
